1a53a2fbc5a4f03eac6294cf8da600eb287434d5
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-04-02 13:34:20 -0400

    contract deployed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/FFVlxm6lqHFICN29zJt0VwYsdbhhZyBC',
+      accounts: ['1248de43436adb7171a7646192a103474d7f87cb2772244f1c397fa14b03ebea']
+    },
+  },
+}
