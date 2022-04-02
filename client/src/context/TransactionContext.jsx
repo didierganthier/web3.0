fcda8c9b6c4f6e042d0eb7987244193a653650cd
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-04-02 17:02:58 -0400

    ETH sent from account 1 to 2

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -12,30 +12,33 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractAbi, signer);
 
-    console.log({
-        provider,
-        signer,
-        transactionContract,
-    })
+    return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
 
-    const [connectedAccount, setConnectedAccount] = useState('')
+    const [connectedAccount, setConnectedAccount] = useState('');
+    const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
+
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+    }
 
     const checkIfWalletConnected = async () => {
         try {
-            if(!ethereum) return alert('Please connect to a wallet');
+            if (!ethereum) return alert('Please connect to a wallet');
 
             const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-            if(accounts.length) {
-            setConnectedAccount(accounts[0]);
+            if (accounts.length) {
+                setConnectedAccount(accounts[0]);
 
-            //getAllTransactions();
-        } else {
-            console.log('No accounts found');
-        }
+                //getAllTransactions();
+            } else {
+                console.log('No accounts found');
+            }
         } catch (error) {
             console.log(error);
 
@@ -45,7 +48,7 @@ export const TransactionProvider = ({ children }) => {
 
     const connectWallet = async () => {
         try {
-            if(!ethereum) return alert('Please install MetaMask');
+            if (!ethereum) return alert('Please install MetaMask');
 
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
@@ -57,12 +60,48 @@ export const TransactionProvider = ({ children }) => {
         }
     }
 
+    const sendTransaction = async () => {
+        try {
+            if (!ethereum) return alert('Please install MetaMask');
+
+            const { addressTo, amount, keyword, message } = formData;
+            const transactionContract = getEthereumContract();
+            const parsedAmount = ethers.utils.parseEther(amount);
+
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: connectedAccount,
+                    to: addressTo,
+                    gas: '0x5208',//21000 GWEI
+                    value: parsedAmount._hex, //0.00001 ETH
+                }],
+            });
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            
+            setTransactionCount(transactionCount.toNumber());
+        } catch (error) {
+            console.log(error);
+
+            throw new Error('No Ethereum object found');
+        }
+    }
+
     useEffect(() => {
         checkIfWalletConnected();
-    },[]);
+    }, []);
 
     return (
-        <TransactionContext.Provider value={{ connectWallet, connectedAccount }}>
+        <TransactionContext.Provider value={{ connectWallet, connectedAccount, formData, setFormData, handleChange, sendTransaction }}>
             {children}
         </TransactionContext.Provider>
     )
