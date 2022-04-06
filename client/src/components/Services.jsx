8cc7de601960cbdbf39b89a632ebe5142fa6965b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-04-05 20:09:08 -0400

    Services.jsx started

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
 
 const Services = () => {
   return (
